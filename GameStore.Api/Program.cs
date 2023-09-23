be7857ffbded03292902695948e86661e3439bae
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-23 16:38:19 -0500

    retract interface for repository add repository to the service

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,10 @@
 using GameStore.Api.Endpoints;
+using GameStore.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddScoped<IGamesRepository, InMemGamesRepository>();
+builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
