e1a8856a8453fdfff5e97a5f666d37fac92e0f60
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-23 21:13:30 -0500

    initialization database when running app

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,7 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,8 @@ var connString = builder.Configuration.GetConnectionString("GameStoreContext");
 builder.Services.AddSqlServer<GameStoreContext>(connString);
 
 var app = builder.Build();
+
+app.Services.InitializeDb();
 
 app.MapGamesEndpoints();
 
